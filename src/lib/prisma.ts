73ba796a7662aa1818668ac8c7bbf2c189463d45
Vasco <jvcs@ua.pt>
2022-10-15 20:54:50 +0100

    :bug: Fixed multiple prisma clients

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
+declare global {
+	var prisma: PrismaClient | undefined;
+}
+
 export async function getPrismaClient(userId: number = -1) {
-	return new PrismaClient();
+	if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+	return global.prisma || new PrismaClient();
 }
