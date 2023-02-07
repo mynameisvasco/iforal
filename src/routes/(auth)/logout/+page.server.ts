570a1dfe1646c7a1c28573d491c17a1ac0b7c4d4
Vasco <jvcs@ua.pt>
2023-02-07 13:02:29 +0000

    :bug: wrong export on logout method

--- src/routes/(auth)/logout/+page.server.ts
+++ src/routes/(auth)/logout/+page.server.ts
@@ -1,6 +1,6 @@
 import { redirect, type RequestEvent } from '@sveltejs/kit';
 
-export async function logout(event: RequestEvent) {
+async function logout(event: RequestEvent) {
 	event.cookies.delete('accessToken');
 	throw redirect(303, '/login');
 }
